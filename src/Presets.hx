f330f65b140fc4d38d986bf7bd956204f5b319e3
Christophe Bessette <christophe.bessette@hotmail.com>
2022-11-08 13:06:34 -0500

    [+] Presets

---
+++ src/Presets.hx
@@ -0,0 +1,8 @@
+class Presets{
+    // tile data
+    public static final TILE_SIZE : Int = 16;
+    // viewport data
+    public static final VIEWPORT_WID : Int = 256;
+    public static final VIEWPORT_HEI : Int = 256;
+        
+}
